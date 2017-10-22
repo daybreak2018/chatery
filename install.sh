31245e7b8d38a2e7cd4671fc398e0f5106841573
Kaustav Banerjee <kaustavsmailbox21@gmail.com>
2017-10-22 21:31:28 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 sudo apt-get install python3.5
 sudo apt-get install python3-pip
 sudo pip3 install virtualenv
-virtualenv -p python3.4 chattery_environment
+virtualenv -p python3.5 chattery_environment
 ./chattery_environment/bin/pip install -r ./requirements.txt
 curl https://getcaddy.com | bash
