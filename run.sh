1efb025c4991b375b45a71a9cbad20b836491246
Kaustav Banerjee <kaustavsmailbox21@gmail.com>
2017-10-22 21:41:04 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-./chattery_environment/bin/python app.py --host 0.0.0.0 --port 8266 --tz ${1:-GMT} &
+./chattery_environment/bin/python ./chatery/app.py --host 0.0.0.0 --port 8266 --tz ${1:-GMT} &
 caddy &
